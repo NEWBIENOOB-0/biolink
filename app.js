9b7d3a156fc291cf5d5cf724b05ffdd8ff1cd81d
NEWBIEN00B <jidnyeshspatil@gmail.com>
2024-02-11 23:39:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,23 +4,19 @@ async function copyText(e) {
     e.preventDefault();
     const link = this.getAttribute('link');
     try {
-        // Check if Web Share API is available
-        if (navigator.share) {
-            // Use Web Share API to open native share dialog
+         if (navigator.share) {
             await navigator.share({
                 title: document.title,
                 text: 'Check out this link:',
                 url: link,
             });
         } else {
-            // Fallback for browsers that do not support Web Share API
-            await navigator.clipboard.writeText(link);
+             await navigator.clipboard.writeText(link);
             alert("Link copied to clipboard.");
         }
     } catch (err) {
         console.error(err);
     }
 }
-
 shareButtons.forEach(shareButton =>
     shareButton.addEventListener('click', copyText));
